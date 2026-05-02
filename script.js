be1239ece52ccaa0fb34e3fe6c9c1eaad7a951f5
arielsalinasnaranjo-ai <arielsalinasnaranjo@gmail.com>
2026-05-02 06:35:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function showMessage() {
+  alert('🌱 ¡Gracias por sumarte al reciclaje!');
+}
